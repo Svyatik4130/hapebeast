ef9b2864b13061886727e4f807b4fbed3ae6a638
Svyatik <barberbi1@icloud.com>
2022-01-03 01:15:53 +0200

    design fixes

--- react-client/src/components/Bird.js
+++ react-client/src/components/Bird.js
@@ -3,16 +3,16 @@ import '../App.css';
 
 const Bird = ({ top, gameRunning }) => {
   const style = { top: `${top}px` };
-  if (!gameRunning) {
+  if (gameRunning) {
     return (
       <div className="Bird" style={style}>
-        <img src="https://cdn.discordapp.com/attachments/927223345370517555/927230454002765834/pixel-hape-2.gif" style={{ width: "70px", height: "70px" }} />
+        <img src="https://dvizhok-hosted-content.s3.us-east-2.amazonaws.com/images/landing/pixel-hape-2.gif" style={{ width: "70px", height: "70px" }} />
       </div>
     )
   } else {
     return (
       <div className="Bird" style={style}>
-        <img src="https://cdn.discordapp.com/attachments/927223345370517555/927230454002765834/pixel-hape-2.gif" style={{ width: "70px", height: "70px" }} />
+        <img src="https://dvizhok-hosted-content.s3.us-east-2.amazonaws.com/images/landing/bird_stay.png" style={{ width: "70px", height: "70px" }} />
       </div>
     )
   }
