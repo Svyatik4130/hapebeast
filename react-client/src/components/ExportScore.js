5fdd6818781074766e6fa70315bc64c5fca7c6d8
Svyatik <barberbi1@icloud.com>
2022-01-04 01:16:43 +0200

    fixed the text

--- react-client/src/components/ExportScore.js
+++ react-client/src/components/ExportScore.js
@@ -9,7 +9,7 @@ export default function ExportScore({ score, gameRunning }) {
     if (!gameRunning && score > 0) {
         return (
             <div onClick={() => {
-                navigator.clipboard.writeText(text); setbtnText("COPIED!"); setTimeout(() => {
+                navigator.clipboard.writeText(text); setbtnText("COPIED! paste it into hapes chat"); setTimeout(() => {
                     setbtnText("Copy my score to clipboard")
                 }, 4000);
             }} className="exp-btn">
